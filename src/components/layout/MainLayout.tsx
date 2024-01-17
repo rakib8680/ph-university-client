9d67ad09d5fe09b5cac3190ea6d091195a929a17
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-17 20:18:35 +0600

    redux broiler plate initialized

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
-import { Layout, Menu, MenuProps } from "antd";
-import { NavLink, Outlet } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import {  Outlet } from "react-router-dom";
 import { adminSideItems } from "../../routes/admin.routes";
 
 const { Header, Content, Footer, Sider } = Layout;
