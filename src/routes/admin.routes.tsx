e1b3a2948c70146a494c81cd590bb2325e0a9395
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-17 17:46:02 +0600

    Dynamic Admin Route Created

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,27 +1,84 @@
+import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
-export const adminPaths = [
+type TAdminPath = {
+  path: string;
+  element: ReactNode;
+};
+
+const adminPaths = [
   {
-    index: true,
-    element: <AdminDashboard />,
-  },
-  {
+    name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
   },
   {
-    path: "create-student",
-    element: <CreateStudent />,
-  },
-  {
-    path: "create-admin",
-    element: <CreateAdmin />,
-  },
-  {
-    path: "create-faculty",
-    element: <CreateFaculty />,
+    name: "User Management",
+    children: [
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+    ],
   },
 ];
+
+export const adminRoutes = adminPaths.reduce((acc: TAdminPath[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+
+
+
+
+// export const adminPaths = [
+//   {
+//     index: true,
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+// ];
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,11 +4,8 @@ import About from "../pages/About";
 import Login from "../pages/Login";
 import Registration from "../pages/Registration";
 import Contact from "../pages/Contact";
-import CreateStudent from "../pages/admin/CreateStudent";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import { adminPaths } from "./admin.routes";
+
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
@@ -28,17 +25,17 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
   {
     path: "/faculty",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
   {
     path: "/student",
     element: <App />,
-    children: adminPaths,
+    children: adminRoutes,
   },
   {
     path: "/login",
