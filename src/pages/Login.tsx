b4cde0f5d1df94f43c19f1027baab9f8a03613eb
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-18 20:39:55 +0600

    Refactor Login component and add useLoginMutation hook

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,40 @@
-
-import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
-
-
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+  const [login, { data, error, isSuccess }] = useLoginMutation();
 
-  const { register, handleSubmit } = useForm();
+  console.log(data, error, isSuccess);
 
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
 
-  const onSubmit = (data)=>{
-    console.log(data);
-  }
+    login(userInfo);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register('userId')} />
+        <input type="text" id="id" {...register("id")} />
       </div>
       <div>
         <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register('password')} />
+        <input type="text" id="password" {...register("password")} />
       </div>
       <Button htmlType="submit">Login</Button>
     </form>
-  )
+  );
 };
 
 export default Login;
