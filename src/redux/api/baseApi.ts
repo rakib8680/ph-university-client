541d2dc8685c09832ed093647997884370added1
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-18 20:19:47 +0600

    endpoints organized to a different folder

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,13 +3,5 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
-  endpoints: (builder) => ({
-    login: builder.mutation({
-      query: (loginInfo) => ({
-        url: "/auth/login",
-        method: "POST",
-        body: loginInfo,
-      }),
-    }),
-  }),
+  endpoints: () => ({}),
 });
---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,19 @@
+import { baseApi } from "../../api/baseApi";
+
+
+
+
+const authApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+          query: (loginInfo) => ({
+            url: "/auth/login",
+            method: "POST",
+            body: loginInfo,
+          }),
+        }),
+      }),
+})
+
+
+export const {useLoginMutation} = authApi
