7cbad5dcb42cec71e247308e5adbdce62855a4f0
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-23 15:30:27 +0600

    Refactor admin page components

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1,10 +1,9 @@
-
 const AdminDashboard = () => {
   return (
-     <div>
-         <h1>This is AdminDashboard component</h1>
-     </div>
-  )
+    <div>
+      <h1>This is AdminDashboard component</h1>
+    </div>
+  );
 };
 
 export default AdminDashboard;
