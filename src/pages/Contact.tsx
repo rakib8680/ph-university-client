43845d79a34f1c135bef5f534dcde018688a7d4f
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-24 15:07:50 +0600

    Refactor Contact and FacultyDashboard components

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,9 @@
-
 const Contact = () => {
   return (
-     <div>
-         <h1>This is Contact component</h1>
-     </div>
-  )
+    <div>
+      <h1>This is Contact component</h1>
+    </div>
+  );
 };
 
 export default Contact;
