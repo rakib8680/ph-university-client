db9c3e214e1598098746f6f659e824164d02bddd
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-07 18:47:33 +0600

    Add Tailwind CSS and PostCSS dependencies

--- src/pages/admin/user management/StudentDetails.tsx
+++ src/pages/admin/user management/StudentDetails.tsx
@@ -1,16 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useGetSingleStudentQuery } from "../../../redux/features/admin/userManagement.api";
+import { TStudent } from "../../../types";
+import { Card } from "antd";
 
 const StudentDetails = () => {
   const { studentId } = useParams();
-  const { data: studentData } = useGetSingleStudentQuery(studentId);
+  const { data } = useGetSingleStudentQuery(studentId);
 
+  const studentData: TStudent = data?.data;
   console.log(studentData);
 
+  const {
+    fullName,
+    email,
+    gender,
+    contactNo,
+    academicDepartment,
+    academicFaculty,
+    admissionSemester,
+    bloogGroup,
+    dateOfBirth,
+    presentAddress,
+    emergencyContactNo,
+    guardian,
+    localGuardian,
+    permanentAddress,
+    isDeleted,
+    id,
+    profileImg,
+    user,
+  } = studentData || {};
+
   return (
-    <div>
-      <h1>This is StudentDetails of {studentId}</h1>
-    </div>
+    <>
+      <Card title={`Information of ${fullName}`} bordered={false}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </>
   );
 };
 
