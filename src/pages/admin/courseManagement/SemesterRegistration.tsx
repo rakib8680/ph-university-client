c61b2b5635b8633a65aa85bd1ec0babd4ce81521
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-08 19:33:55 +0600

    Add semester status options and update SemesterRegistration component

--- src/pages/admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/admin/courseManagement/SemesterRegistration.tsx
@@ -1,9 +1,40 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import PHInput from "../../../components/form/PHInput";
+import PHDatePicker from "../../../components/form/PHDatePicker";
 
 const SemesterRegistration = () => {
+
+
+
+
+  
+
   return (
-     <div>
-         <h1>This is SemesterRegistration component</h1>
-     </div>
+    <Flex justify="center" align="center">
+    <Col span={6}>
+      <PHForm onSubmit={onSubmit}>
+        <PHSelect
+          label="Academic Semester"
+          name="academicSemester"
+          options={academicSemesterOptions}
+        />
+
+        <PHSelect
+          name="status"
+          label="Status"
+          options={semesterStatusOptions}
+        />
+        <PHDatePicker name="startDate" label="Start Date" />
+        <PHDatePicker name="endDate" label="End Date" />
+        <PHInput type="text" name="minCredit" label="Min Credit" />
+        <PHInput type="text" name="maxCredit" label="Max Credit" />
+
+        <Button htmlType="submit">Submit</Button>
+      </PHForm>
+    </Col>
+  </Flex>
   )
 };
 
