730e864c78cbca3e3ffb23a437fde5ea6c10e743
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-09 15:36:27 +0600

    Create Course api static form created

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -4,28 +4,32 @@ import { Controller } from "react-hook-form";
 type TPHSelectProps = {
   label: string;
   name: string;
-  options: {
-    value: string;
-    label: string;
-    disabled?: boolean;
-  }[] | undefined;
+  options:
+    | {
+        value: string;
+        label: string;
+        disabled?: boolean;
+      }[]
+    | undefined;
   disabled?: boolean;
+  mode?: "multiple" | undefined;
 };
 
-const PHSelect = ({ label, name, options , disabled}: TPHSelectProps) => {
+const PHSelect = ({ label, name, options, disabled, mode }: TPHSelectProps) => {
   return (
     <Controller
       name={name}
-      render={({ field , fieldState: {error}}) => (
+      render={({ field, fieldState: { error } }) => (
         <Form.Item label={label}>
           <Select
+            mode={mode}
             style={{ width: "100%" }}
             {...field}
             options={options}
             disabled={disabled}
             size="large"
           />
-          {error && <p style={{color: "red"}}>{error.message}</p>}
+          {error && <p style={{ color: "red" }}>{error.message}</p>}
         </Form.Item>
       )}
     />
--- src/pages/admin/courseManagement/CreateCourse.tsx
+++ src/pages/admin/courseManagement/CreateCourse.tsx
@@ -1,10 +1,36 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { FieldValues } from "react-hook-form";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateCourse = () => {
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-     <div>
-         <h1>This is CreateCourse component</h1>
-     </div>
-  )
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="title" label="Title" />
+          <PHInput type="text" name="prefix" label="Prefix" />
+          <PHInput type="text" name="code" label="Code" />
+          <PHInput type="text" name="credits" label="Credits" />
+          <PHSelect
+            options={[
+              { value: "rakib", label: "xd" },
+              { value: "rakib2", label: "lol" },
+            ]}
+            name="preRequisiteCourses"
+            label="Pre-requisite Courses"
+            mode="multiple"
+          />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateCourse;
