2c83727c96566cf58ef1ba4ca4c1fcfa54202668
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-09 17:48:30 +0600

    Add table with course data

--- src/pages/admin/courseManagement/Courses.tsx
+++ src/pages/admin/courseManagement/Courses.tsx
@@ -1,10 +1,47 @@
+import { Button, Table, TableColumnsType } from "antd";
+import { useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagement.api";
+import { TCourse } from "../../../types";
+
+type TTableData = Pick<TCourse, "title" | "code">;
 
 const Courses = () => {
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+  const tableData = courses?.data?.map(({ _id, title, code, prefix }) => ({
+    key: _id,
+    title,
+    code: `${prefix}${code}`,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Title",
+      key: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "Code",
+      key: "code",
+      dataIndex: "code",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return <Button>Assign Faculty</Button>;
+      },
+    },
+  ];
+
   return (
-     <div>
-         <h1>This is Courses component</h1>
-     </div>
-  )
+    <Table
+      columns={columns}
+      loading={isFetching}
+      dataSource={tableData}
+      pagination={false}
+      // onChange={onChange}
+    />
+  );
 };
 
 export default Courses;
