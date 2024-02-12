64d0a58a40d7d3c4705e9cd7e22cbbe0a9102147
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-12 18:27:58 +0600

    Refactor MySchedule component to display enrolled courses

--- src/pages/student/MySchedule.tsx
+++ src/pages/student/MySchedule.tsx
@@ -1,17 +1,25 @@
 import { useGetAllEnrolledCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
 
 const MySchedule = () => {
-
-    const {data:enrolledCourses} = useGetAllEnrolledCoursesQuery(undefined);
-    
-    console.log(enrolledCourses);
-
+  const { data } = useGetAllEnrolledCoursesQuery(undefined);
 
   return (
-     <div>
-         <h1>This is MySchedule component</h1>
-     </div>
-  )
+    <div>
+      {data?.data?.map((item) => {
+        return (
+          <div key={item._id}>
+            <div>{item.course.title}</div>
+            <div>{item.offeredCourse.section}</div>
+            <div>
+              {item.offeredCourse.days.map((item, index) => (
+                <span key={index}> {item}</span>
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MySchedule;
